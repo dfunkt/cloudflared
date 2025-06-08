48cdd5f874ba6e8999e5a9f4a69ab80449751701
dfunkt <daniel.barabasa@gmail.com>
2025-06-08 09:30:59 +0300

    Switch to personal fork of quic-go

--- supervisor/tunnel.go
+++ supervisor/tunnel.go
@@ -605,7 +605,7 @@ func (e *EdgeTunnelServer) serveQUIC(
 	if connOptions.FeatureSnapshot.DatagramVersion == features.DatagramV3 {
 		datagramSessionManager = connection.NewDatagramV3Connection(
 			ctx,
-			conn,
+			conn.UnderlyingConn(),
 			e.sessionManager,
 			e.config.ICMPRouterServer,
 			connIndex,
@@ -615,7 +615,7 @@ func (e *EdgeTunnelServer) serveQUIC(
 	} else {
 		datagramSessionManager = connection.NewDatagramV2Connection(
 			ctx,
-			conn,
+			conn.UnderlyingConn(),
 			e.config.OriginDialerService,
 			e.config.ICMPRouterServer,
 			connIndex,
@@ -629,7 +629,7 @@ func (e *EdgeTunnelServer) serveQUIC(
 	// Wrap the [quic.Connection] as a TunnelConnection
 	tunnelConn, err := connection.NewTunnelConnection(
 		ctx,
-		conn,
+		conn.UnderlyingConn(),
 		connIndex,
 		e.orchestrator,
 		datagramSessionManager,
